5925492f630dc0bf9a67c3b34686d20a55abf23e
limpid-kzonix <balyszyn@gmail.com>
2022-05-02 23:16:35 +0300

    improve jvm options

---
+++ extra/project-templating/main.py
@@ -0,0 +1,6 @@
+## TODO: implement coockiecutter project templating instead of g8 project-templating
+##  The main benefit is the generation time (avoidance of sbt sources compilation)
+##  The cookiecutter provides possibility to define custom python script with pre/post hooks
+##  in order to add additional step around project generation.
+##  It also provides mechanism to generate specific files within existing project
+
